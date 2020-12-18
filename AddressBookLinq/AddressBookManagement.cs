ed375ff4e89b07eb1ee4aeeff3f81b3f1c195cb0
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-18 19:40:20 +0530

    ADDED:Created tabletable to add the values in the dataTable

---
+++ AddressBookLinq/AddressBookManagement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookLinq
+{
+    class AddressBookManagement
+    {
+        //uc1 creating DataTable
+        DataTable dataTable = new DataTable();
+    }
+}
