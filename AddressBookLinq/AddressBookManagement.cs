6c0522d06468d7d8daaf4329aa25043ac8880b1f
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-18 19:47:13 +0530

    ADDED:Columns to dataTable

--- AddressBookLinq/AddressBookManagement.cs
+++ AddressBookLinq/AddressBookManagement.cs
@@ -9,5 +9,16 @@ namespace AddressBookLinq
     {
         //uc1 creating DataTable
         DataTable dataTable = new DataTable();
+        public AddressBookManagement()
+        {
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("Zip", typeof(string));
+            dataTable.Columns.Add("PhoneNumber", typeof(string));
+            dataTable.Columns.Add("EmailId", typeof(string));
+        }
     }
 }
