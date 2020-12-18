e18827392df8ff135119cf61190456558b8520ea
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-18 19:33:48 +0530

    STARTED:AddressBookUsingLinq

---
+++ AddressBookLinq/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBookLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AddressBook using Linq");
+            Console.ReadKey();
+        }
+    }
+}
